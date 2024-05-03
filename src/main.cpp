2558f9267d4cec01ce293e417b70d73cdfe8dd9c
TomasLopezTur <tomas.lopez.tur@gmail.com>
2024-05-03 16:56:34 -0300

    prueba de servo

--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,49 @@
 #include <Arduino.h>
-#include <Servo.h>
-#include <Stepper.h>
+#include <ESP32Servo.h>
+/* #include <Servo.h> */
+/* #include <Stepper.h> */
 
 // Definición de pines para los motores
-const int microServoPin = 13;
+/* const int microServoPin = 13; */
 const int servo2Pin = 12;
 const int servo3Pin = 14;
-const int stepper1Pin1 = 27;
+/* const int stepper1Pin1 = 27;
 const int stepper1Pin2 = 26;
 const int stepper2Pin1 = 25;
-const int stepper2Pin2 = 33;
+const int stepper2Pin2 = 33; */
 
 //Servos y steppers
-Servo microServo1;
+/* Servo microServo1;
 Stepper stepper1(200, stepper1Pin1, stepper1Pin2);
-Stepper stepper2(200, stepper2Pin1, stepper2Pin2);
+Stepper stepper2(200, stepper2Pin1, stepper2Pin2); */
 Servo servo2;
 Servo servo3;
 
 void setup() {
   // put your setup code here, to run once:
   
-  microServo1.attach(6);
+/*   microServo1.attach(6); */
   servo2.attach(7);
   servo3.attach(8);
 
-  microServo1.write(90);
+  /* microServo1.write(90); */
   servo2.write(90);
   servo3.write(90);
 
-  // Mueve el stepper motor a 90 grados (en sentido horario)
+/*   // Mueve el stepper motor a 90 grados (en sentido horario)
   int stepsToMove1 = 90 * 200 / 360;  // Calcula la cantidad de pasos para 90 grados
   stepper1.step(stepsToMove1); 
   int stepsToMove2 = 90 * 200 / 360;  // Calcula la cantidad de pasos para 90 grados
-  stepper2.step(stepsToMove2);
+  stepper2.step(stepsToMove2); */
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for (int i = 90; i <= 100; i++)
+  {
+    servo2.write(i++);
+    /* servo3.write(90); */
+  }
+  
 }
 
